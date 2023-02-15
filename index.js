e6c24450c768911d1f8f6b597afd6b4ff6df63ae
Abhishek Anand <anand2000ys@gmail.com>
2023-02-15 15:19:22 +0530

    osmodule

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ const supervillains = require('supervillains');
 var mySuperVillainsName = supervillains.random();
 
 console.log(mySuperVillainsName);
+
+const abhishek = require('./second');
+
+console.log(abhishek);
---
+++ second.js
@@ -0,0 +1,9 @@
+second = {
+    name: "Abhishek",
+    favNum: 7,
+    developer: true
+}
+
+module.exports = second;
+
+console.log(exports, require, module, __filename, __dirname);
